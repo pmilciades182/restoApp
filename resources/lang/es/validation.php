7a687fa0e0465831e371bf74927636c06536ef2b
pagonzal <pgonzalez@nsa.com.py>
2025-01-10 14:07:44 -0300

    cambio en traduccion

---
+++ resources/lang/es/validation.php
@@ -0,0 +1,15 @@
+<?php
+// En resources/lang/es/validation.php
+return [
+    'required' => 'El campo :attribute es obligatorio.',
+    'string' => 'El campo :attribute debe ser una cadena de texto.',
+    'max' => [
+        'string' => 'El campo :attribute no debe ser mayor que :max caracteres.',
+    ],
+
+    'attributes' => [
+        'name' => 'nombre',
+        'description' => 'descripción',
+        // otros atributos...
+    ],
+];
