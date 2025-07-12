e73cedafbbc5a1b071f06951e6895dfb6ee0d278
pmilciades182 <38768141+pmilciades182@users.noreply.github.com>
2025-07-12 16:23:17 -0300

    Revert "feat: improve Docker setup with robust configuration"

--- docker/mysql/init/01-create-testing-db.sql
+++
@@ -1,6 +0,0 @@
--- Create testing database for Laravel tests
-CREATE DATABASE IF NOT EXISTS `testing` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Grant privileges to application user for testing database
-GRANT ALL PRIVILEGES ON `testing`.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
